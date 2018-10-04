601f4da213e6caf9bf558c7ddb416ced851d8467
ahuglajbclajep <ahuglajbclajep@gmail.com>
2018-10-04 14:36:21 +0900

    install yapf

---
+++ README.md
@@ -0,0 +1,7 @@
+# django-sandbox
+
+Sandbox of [Django](https://www.djangoproject.com).
+
+```sh
+pipenv install -d && chmod a+x pre-commit.sh && mkdir -p .git/hooks && cp pre-commit.sh .git/hooks/pre-commit
+```
---
+++ pre-commit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script is based on https://github.com/google/yapf/blob/39e0290b2b0f9f678122391d29da6733081b99b9/plugins/pre-commit.sh.
+
+# Find all added or modified staged Python files.
+PYTHON_FILES=(`git diff --name-only --cached --diff-filter=AM | grep --color=never '.py$'`)
+if [ ! "$PYTHON_FILES" ]; then
+  exit 0
+fi
+
+# Check for existence of yapf.
+if [ -z $(pipenv run which yapf) ]; then
+  echo 'yapf not on path; can not format. Please install yapf:'
+  echo '    pipenv install -d'
+  exit 2
+fi
+
+# Format Python files, and add these.
+echo 'Formatting staged Python files . . .'
+pipenv run yapf -i -r ${PYTHON_FILES[@]}
+git add ${PYTHON_FILES[@]}
+exit 0
